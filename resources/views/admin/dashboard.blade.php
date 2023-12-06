ca26602b86b9b3a1a13245f9717439a224dabc26
GuilhermeAlamino <guilhermedevelopedweb@hotmail.com>
2023-12-06 16:27:16 -0300

    updated in secret

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -36,7 +36,6 @@
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
-            
             <div class="col-xl-4 col-md-6 mb-4">
                 <div class="card border-left-success shadow h-100 py-2">
                     <div class="card-body">
